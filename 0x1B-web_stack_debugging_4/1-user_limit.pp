0792e64b4cd75b767f9d13f1485c811a6189c75e
anas-hfd <anas1.hfd@gmail.com>
2023-11-15 18:33:21 +0100

    no limits for the su

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# logging in holberton without error
+
+# increase holberton hard file limit
+exec { 'increase-hard-file-limit':
+  command => "sed -i '/^holberton hard/s/5/90000/' /etc/security/limits.conf",
+  path    => 'user/local/bin/:/bin/',
+}
+
+# increase soft file limit
+exec { 'increase-soft file limit':
+  command => "sed -i '/^holberton soft/s/4/90000/' /etc/security/limits.conf",
+  path    => 'user/local/bin/:/bin/',
+}
