ea874a506e771cbccd9896d9cb847e9e13a89e00
anas-hfd <anas1.hfd@gmail.com>
2023-11-15 18:00:51 +0100

    the sky is not the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,15 @@
-#increasing the amount of requests (traffic) handled by nginx (soft and hard limits).
+# increasing the amount of requests (traffic) handled by nginx (soft and hard limits).
+
+# increasing ULIMIT of default file
+exec { 'fix--for-nginx':
+  #ULIMIT value
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  #specifying the path
+  path    => 'user/local/bin/:/bin/',
+}
+
+# Restart service nginx
+exec { 'nginx-restart':
+  command => 'etc/init.d/nginx restart',
+  path    => 'etc/init.d/',
+}
